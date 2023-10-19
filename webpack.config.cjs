8fb18bc9b58ec857f6dc15416a6b566be58a6dd6
aleksei-shvetz <al.sv.dev@gmail.com>
2023-10-19 15:09:35 +0300

    Create webpack configuration

---
+++ webpack.config.cjs
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-unresolved */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    filename: '[name].[contenehash].js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
+
+  devServer: {
+    port: 5000,
+    hot: true,
+    open: true,
+  },
+};
