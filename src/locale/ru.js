6a2591f7ce3fca2e8a49cecf51ebacf08fdde8ec
aleksei-shvetz <al.sv.dev@gmail.com>
2023-10-28 11:32:36 +0300

    Add file witt texts, integration i18next

---
+++ src/locale/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    feedback: {
+      uploadRss: 'RSS успешно загружен',
+    },
+    errors: {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+    },
+    mainTitle: 'RSS агрегатор',
+    subTitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    inputHint: 'Ссылка RSS',
+    exampleLink: 'Пример: https://www.vedomosti.ru/rss/news',
+    createdBy: 'created by',
+    myName: 'Aleksei Shvets',
+  },
+};
--- src/view.js
+++ src/view.js
@@ -1,6 +1,23 @@
 import onChange from 'on-change';
-
 import * as yup from 'yup';
+import i18next from 'i18next';
+import ru from './locale/ru.js';
+
+const feedbackEl = document.querySelector('.feedback');
+const inputEl = document.getElementById('url-input');
+const formEl = document.querySelector('.rss-form');
+
+/*
+const headTitileEl = document.querySelector('h1');
+const subTitleEl = document.querySelector('.lead');
+const labelEl = document.querySelector('[for="url-input"]');
+const exampleEl = formEl.nextElementSibling();
+
+headTitileEl.textContent = i18next.t('mainTitle');
+subTitleEl.textContent = i18next.t('subTitle');
+labelEl.textContent = i18next.t('inputHint');
+exampleEl.textContent = i18next.t('examplLink');
+*/
 
 const state = {
   linkValidation: 'none',
@@ -10,22 +27,33 @@ const state = {
   },
 };
 
+i18next.init({
+  lng: 'ru',
+  debug: true,
+  resources: {
+    ru,
+  },
+});
+
 const schema = yup.string().url();
-const feedbackEl = document.querySelector('.feedback');
 
 const feedbackRender = (currentStatus) => {
   if (currentStatus === 'none') {
     feedbackEl.textContent = '';
   }
   if (currentStatus === 'valid') {
-    feedbackEl.textContent = 'RSS успешно загружен';
+    feedbackEl.textContent = i18next.t('feedback.uploadRss');
     feedbackEl.classList.remove('text-danger');
     feedbackEl.classList.add('text-success');
+    inputEl.classList.remove('is-invalid');
+    inputEl.classList.add('is-valid');
   }
   if (currentStatus === 'invalid') {
-    feedbackEl.textContent = 'Ссылка должна быть валидным URL';
+    feedbackEl.textContent = i18next.t('errors.invalidUrl');
     feedbackEl.classList.add('text-danger');
     feedbackEl.classList.remove('text-success');
+    inputEl.classList.add('is-invalid');
+    inputEl.classList.remove('is-valid');
   }
 };
 
@@ -36,8 +64,6 @@ const watcherValidation = onChange(state, (path, current) => {
 });
 
 export default () => {
-  const inputEl = document.getElementById('url-input');
-  const formEl = document.querySelector('.rss-form');
   formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     const link = inputEl.value;
@@ -46,6 +72,11 @@ export default () => {
       .then((data) => {
         const status = data ? 'valid' : 'invalid';
         watcherValidation.linkValidation = status;
+        if (data) {
+          inputEl.value = '';
+        } else {
+          inputEl.value = link;
+        }
       });
   });
 };
