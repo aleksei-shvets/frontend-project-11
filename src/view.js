669280810db58eedd3aa35402c07b47648ec55a6
aleksei-shvetz <al.sv.dev@gmail.com>
2023-11-02 21:17:31 +0300

    Add rendering feeds and posts to document

--- src/view.js
+++ src/view.js
@@ -13,8 +13,8 @@ const state = {
   errors: [],
 };
 
-const feeds = [];
-const posts = [];
+const feedsState = [];
+const postsState = [];
 
 i18next.init({
   lng: 'ru',
@@ -32,6 +32,86 @@ const errorsRender = () => {
     feedbackEl.classList.add('text-danger');
     feedbackEl.classList.remove('text-success');
   }
+};
+
+const feedsRender = () => {
+  const feedsContainer = document.querySelector('.feeds');
+
+  const feedsColumn = document.createElement('div');
+  feedsColumn.classList.add('card', 'border-0');
+
+  const feedTitleDiv = document.createElement('div');
+  feedTitleDiv.classList.add('card-body');
+
+  const feedsBlockTitle = document.createElement('h2');
+  feedsBlockTitle.classList.add('card-title', 'h4');
+  feedsBlockTitle.textContent = i18next.t('feedTitle');
+
+  const feeds = document.createElement('ul');
+  feeds.classList.add('list-group', 'border-0', 'rounded-0');
+
+  feedTitleDiv.append(feedsBlockTitle);
+  feedsColumn.append(feedTitleDiv, feeds);
+  feedsContainer.append(feedsColumn);
+  //feedsContainer.append(feeds);
+
+  feedsState.forEach((feed) => {
+    const feedItem = document.createElement('li');
+    feedItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const feedTitle = document.createElement('h3');
+    feedTitle.classList.add('h6', 'm-0');
+    const feedDescription = document.createElement('p');
+    feedDescription.classList.add('m-0', 'small', 'text-black-50');
+    feedTitle.textContent = feed.feedTitle;
+    feedDescription.textContent = feed.feedDescription;
+    feedItem.append(feedTitle, feedDescription);
+    feeds.append(feedItem);
+  });
+};
+
+const postsRender = () => {
+  const postsContainer = document.querySelector('.posts');
+
+  const postsColumn = document.createElement('div');
+  postsColumn.classList.add('card', 'border-0');
+
+  const postsTitleDiv = document.createElement('div');
+  postsTitleDiv.classList.add('card-body');
+
+  const postsBlockTitle = document.createElement('h2');
+  postsBlockTitle.classList.add('card-title', 'h4');
+  postsBlockTitle.textContent = i18next.t('postTitle');
+
+  postsTitleDiv.append(postsBlockTitle);
+
+  const posts = document.createElement('ul');
+  posts.classList.add('list-group', 'border-0', 'rounded-0');
+
+  postsColumn.append(postsTitleDiv);
+  postsColumn.append(posts);
+  postsContainer.append(postsColumn);
+
+  postsState.forEach((item) => {
+    item.feedPosts.forEach((post) => {
+      const postItem = document.createElement('li');
+      postItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+      const linkName = document.createElement('a');
+      linkName.textContent = post.postTitle;
+      linkName.classList.add('fw-bold');
+      linkName.setAttribute('target', '_blank');
+      linkName.setAttribute('rel', 'noopener noreferrer');
+      linkName.href = post.postLink;
+
+      const button = document.createElement('button');
+      button.textContent = 'Просмотр';
+      button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+      button.setAttribute('type', 'button');
+      button.setAttribute('rel', 'noopener noreferrer');
+
+      postItem.append(linkName, button);
+      posts.append(postItem);
+    });
+  });
 };
 
 const feedbackRender = (currentStatus) => {
@@ -57,30 +137,39 @@ const watcherValidation = onChange(state, (path, current) => {
   }
 });
 
+const feedsWatcher = onChange(feedsState, () => {
+  feedsRender();
+});
+
+const postsWatcher = onChange(postsState, () => {
+  postsRender();
+});
+
 const parser = (xmlString) => {
   const newParser = new DOMParser();
   const doc = newParser.parseFromString(xmlString, 'application/xhtml+xml');
   const postItems = doc.querySelectorAll('channel > item');
-  const feedName = doc.querySelector('channel > title').textContent;
-  const feedLink = doc.querySelector('channel > link').textContent;
+  const feedTitle = doc.querySelector('channel > title').textContent;
+  const feedDescription = doc.querySelector('channel > description').textContent;
+  const feedPosts = [];
   postItems.forEach((post) => {
     const postTitle = post.querySelector('title').textContent;
     const postLink = post.querySelector('link').textContent;
     const postDescription = post.querySelector('description').textContent;
     const postCategory = (post.querySelector('category')) ? post.querySelector('category').textContent : '';
-    posts.push({
+    feedPosts.push({
       postTitle,
       postDescription,
       postCategory,
       postLink,
     });
   });
-  return [feedName, feedLink, posts];
+  return [feedTitle, feedDescription, feedPosts];
 };
 
 export default () => {
   let idCounter;
-  if (feeds.length === 0) {
+  if (feedsState.length === 0) {
     idCounter = 0;
   }
   const headTitileEl = document.querySelector('h1');
@@ -112,23 +201,24 @@ export default () => {
         inputEl.value = link;
         return;
       }
-      const [feedName, feedLink, feedPosts] = parser(res.data.contents);
+      const [feedTitle, feedDescription, feedPosts] = parser(res.data.contents);
 
-      feeds.forEach((item) => {
-        if (item.feedName === feedName) {
+      feedsState.forEach((item) => {
+        if (item.feedName === feedTitle) {
           watcherValidation.errors.push('doubledChannel');
-          return;
+          // return;
         }
       });
       const feedId = idCounter;
       idCounter += 1;
-      feeds.push({ feedId, feedName, feedLink });
-      posts.push({ feedId, feedPosts });
+      feedsWatcher.push({ feedId, feedTitle, feedDescription });
+      postsWatcher.push({ feedId, feedPosts });
       watcherValidation.linkValidation = 'valid';
-    }).catch(() => {
+    }).catch((err) => {
+      console.log(err);
       watcherValidation.errors.push('notConnected');
     });
-    console.log(feeds);
-    console.log(posts);
+    console.log(feedsState);
+    console.log(postsState);
   });
 };
