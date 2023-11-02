1642d45b104583266e111bfc05b1c57b49f739bc
aleksei-shvetz <al.sv.dev@gmail.com>
2023-11-02 11:47:52 +0300

    Add logic
    -added load fedds and posts
    -added error processing

--- src/view.js
+++ src/view.js
@@ -1,32 +1,20 @@
 import onChange from 'on-change';
 import * as yup from 'yup';
 import i18next from 'i18next';
-import ru from './locale/ru.js';
 import axios from 'axios';
+import ru from './locale/ru.js';
 
 const feedbackEl = document.querySelector('.feedback');
 const inputEl = document.getElementById('url-input');
 const formEl = document.querySelector('.rss-form');
 
-/*
-const headTitileEl = document.querySelector('h1');
-const subTitleEl = document.querySelector('.lead');
-const labelEl = document.querySelector('[for="url-input"]');
-const exampleEl = formEl.nextElementSibling();
-
-headTitileEl.textContent = i18next.t('mainTitle');
-subTitleEl.textContent = i18next.t('subTitle');
-labelEl.textContent = i18next.t('inputHint');
-exampleEl.textContent = i18next.t('examplLink');
-*/
-
 const state = {
   linkValidation: 'none',
-  channels: {
-    feeds: [],
-    feedLinks: [],
-  },
+  errors: [],
 };
+
+const feeds = [];
+const posts = [];
 
 i18next.init({
   lng: 'ru',
@@ -37,6 +25,14 @@ i18next.init({
 });
 
 const schema = yup.string().url();
+
+const errorsRender = () => {
+  if (state.errors.length > 0) {
+    feedbackEl.textContent = i18next.t(`errors.${state.errors}`);
+    feedbackEl.classList.add('text-danger');
+    feedbackEl.classList.remove('text-success');
+  }
+};
 
 const feedbackRender = (currentStatus) => {
   if (currentStatus === 'none') {
@@ -49,44 +45,90 @@ const feedbackRender = (currentStatus) => {
     inputEl.classList.remove('is-invalid');
     inputEl.classList.add('is-valid');
   }
-  if (currentStatus === 'invalid') {
-    feedbackEl.textContent = i18next.t('errors.invalidUrl');
-    feedbackEl.classList.add('text-danger');
-    feedbackEl.classList.remove('text-success');
-    inputEl.classList.add('is-invalid');
-    inputEl.classList.remove('is-valid');
-  }
 };
 
 const watcherValidation = onChange(state, (path, current) => {
   if (path === 'linkValidation') {
     feedbackRender(current);
   }
+  if (path === 'errors') {
+    feedbackRender(current);
+    current.forEach(() => errorsRender());
+  }
 });
 
+const parser = (xmlString) => {
+  const newParser = new DOMParser();
+  const doc = newParser.parseFromString(xmlString, 'application/xhtml+xml');
+  const postItems = doc.querySelectorAll('channel > item');
+  const feedName = doc.querySelector('channel > title').textContent;
+  const feedLink = doc.querySelector('channel > link').textContent;
+  postItems.forEach((post) => {
+    const postTitle = post.querySelector('title').textContent;
+    const postLink = post.querySelector('link').textContent;
+    const postDescription = post.querySelector('description').textContent;
+    const postCategory = (post.querySelector('category')) ? post.querySelector('category').textContent : '';
+    posts.push({
+      postTitle,
+      postDescription,
+      postCategory,
+      postLink,
+    });
+  });
+  return [feedName, feedLink, posts];
+};
+
 export default () => {
+  let idCounter;
+  if (feeds.length === 0) {
+    idCounter = 0;
+  }
+  const headTitileEl = document.querySelector('h1');
+  const subTitleEl = document.querySelector('.lead');
+  const labelEl = document.querySelector('[for="url-input"]');
+  const exampleEl = document.getElementById('example');
+
+  headTitileEl.textContent = i18next.t('mainTitle');
+  subTitleEl.textContent = i18next.t('subTitle');
+  labelEl.textContent = i18next.t('inputHint');
+  exampleEl.textContent = i18next.t('exampleLink');
+
   formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     const link = inputEl.value;
 
     const response = schema.isValid(link)
       .then((data) => {
-        const status = data ? 'valid' : 'invalid';
-        watcherValidation.linkValidation = status;
-        if (data) {
-          inputEl.value = '';
-          return axios.get(link);
+        if (!data) {
+          watcherValidation.errors.push('invalidUrl');
+          return link;
         }
-        return link;
+        inputEl.value = '';
+        return axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${link}`);
       });
+
     response.then((res) => {
       if (res === link) {
         inputEl.value = link;
         return;
       }
-      if (res.status >= 200 && res.status < 300) {
-        console.log(res.data);
-      }
+      const [feedName, feedLink, feedPosts] = parser(res.data.contents);
+
+      feeds.forEach((item) => {
+        if (item.feedName === feedName) {
+          watcherValidation.errors.push('doubledChannel');
+          return;
+        }
+      });
+      const feedId = idCounter;
+      idCounter += 1;
+      feeds.push({ feedId, feedName, feedLink });
+      posts.push({ feedId, feedPosts });
+      watcherValidation.linkValidation = 'valid';
+    }).catch(() => {
+      watcherValidation.errors.push('notConnected');
     });
+    console.log(feeds);
+    console.log(posts);
   });
 };
