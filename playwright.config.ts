970cfbbdbfd7c54c3ebcf483b74663251ea7e396
aleksei-shvetz <al.sv.dev@gmail.com>
2023-11-20 22:22:28 +0300

    Create configs for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  expect: { timeout: 25000 },
 
   /* Configure projects for major browsers */
   projects: [
@@ -42,10 +43,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //  name: 'webkit',
+    //  use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
