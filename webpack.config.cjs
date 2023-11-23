505a344432c1060906e821ccabcf04c770abb7e9
aleksei-shvetz <al.sv.dev@gmail.com>
2023-11-23 17:19:48 +0300

    Change dist to public

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
     filename: '[name].[contenehash].js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './public'),
     clean: true,
   },
 
