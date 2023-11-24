ee1c6a7471991b9d076a6ae182fc48cf512bbf1c
aleksei-shvetz <al.sv.dev@gmail.com>
2023-11-24 17:38:08 +0300

    Refactoring feedbackMessageRender

--- src/feedbackMessageRender.js
+++ src/feedbackMessageRender.js
@@ -2,6 +2,17 @@ import { changesClasses } from './functions.js';
 
 export default (currentStatus, staticElements, uploadRssMessage) => {
   const { feedbackEl, inputEl } = staticElements;
+  const validation = {
+    none: () => {
+      feedbackEl.textContent = '';
+    },
+    valid: () => {
+      feedbackEl.textContent = uploadRssMessage;
+      changesClasses(feedbackEl, ['text-danger'], ['text-success']);
+      changesClasses(inputEl, ['is-invalid'], ['is-valid']);
+    },
+  };
+  /*
   if (currentStatus === 'none') {
     feedbackEl.textContent = '';
   }
@@ -10,4 +21,6 @@ export default (currentStatus, staticElements, uploadRssMessage) => {
     changesClasses(feedbackEl, ['text-danger'], ['text-success']);
     changesClasses(inputEl, ['is-invalid'], ['is-valid']);
   }
+  */
+  return validation[currentStatus]();
 };
