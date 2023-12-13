b5afcc5b84d75ba7b7f04944bdaa26a522af02c1
aleksei-shvetz <al.sv.dev@gmail.com>
2023-12-13 10:23:56 +0300

    refactoring texts

--- src/locale/ru.js
+++ src/locale/ru.js
@@ -11,17 +11,11 @@ export default {
       unknownError: 'Что-то пошло не так',
       emptyField: 'Не должно быть пустым',
     },
-    mainTitle: 'RSS агрегатор',
-    subTitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
-    inputHint: 'Ссылка RSS',
-    exampleLink: 'Пример: https://ru.hexlet.io/lessons.rss',
     createdBy: 'created by',
     myName: 'Aleksei Shvets',
     postTitle: 'Посты',
     feedTitle: 'Фиды',
-    add: 'Добавить',
     viewing: 'Просмотр',
-    closeBtn: 'Закрыть',
-    readBtn: 'Читать полностью',
+
   },
 };
